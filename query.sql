04c5835dd962e82abcd560ae7b65e262407dad4e
OrlandoTellez <espadachinkinno@gmail.com>
2025-09-14 22:05:05 -0600

    update: actualizar query

--- query.sql
+++ query.sql
@@ -13,8 +13,23 @@ CREATE DATABASE db_paciente_app
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
+-- Tabla de hospitales
 CREATE TABLE hospitals (
     id_hospital SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     address VARCHAR(200) NOT NULL
+);
+
+-- Tabla de pacientes
+CREATE TABLE patients (
+    id_patient SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    second_name VARCHAR(50),
+    first_lastname VARCHAR(50) NOT NULL,
+    second_lastname VARCHAR(50),
+    birthdate DATE NOT NULL,
+    phone VARCHAR(15),
+    address VARCHAR(300),
+    email VARCHAR(100) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
 );
