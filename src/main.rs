edfc2cbc9305f0c2f1704e3b432461358b77ca61
OrlandoTellez <espadachinkinno@gmail.com>
2025-09-23 18:53:31 -0600

    feat: agregar archivo .env para variables de entorno y usar dotenvy para usarlo en el proyecto

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,18 @@ mod routes;
 
 use sqlx::postgres::PgPoolOptions;
 use axum::Router;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL no está definida en .env");
+    let app_port = env::var("APP_PORT").unwrap_or_else(|_| "3000".to_string());
+
     let pool = PgPoolOptions::new()
-        .connect("postgresql://postgres:espadaLINUX@localhost:5432/db_paciente_app")
+        .connect(&database_url)
         .await
         .expect("Error conectando a la BD");
 
@@ -21,9 +28,11 @@ async fn main() {
         .merge(routes::user::routes_user(pool.clone()))
         .merge(routes::doctor::routes_doctor(pool.clone()))
         .merge(routes::services::routes_services(pool.clone()))
-        .merge(routes::speciality::routes_speciality(pool.clone()))
-        ;
+        .merge(routes::speciality::routes_speciality(pool.clone()));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let addr = format!("0.0.0.0:{}", app_port);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    println!("Servidor iniciado en http://{}", addr);
+
     axum::serve(listener, app).await.unwrap();
 }
